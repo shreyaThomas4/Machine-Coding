a0b413738b564cb9aa2edbaaea25073b76c5f6e0
shreya.thomas <shreya.thomas18@gmail.com>
2025-03-15 17:38:53 +0530

    Adds React app code for pagination based on front end and backend

---
+++ machine-coding/README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+This repository consists of machine coding questions in front end
+
+1. Pagination (Both FrontEnd and BackEnd Approach)
---
+++ machine-coding/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import PaginationFEApproach from './Pagination/PaginationFEApproach';
+import PaginationBEApproach from './Pagination/PaginationBEApproach';
+
+function App() {
+
+  return (
+    <>
+      {/* <PaginationFEApproach/> */}
+      <PaginationBEApproach />
+
+
+    </>
+  )
+}
+
+export default App
